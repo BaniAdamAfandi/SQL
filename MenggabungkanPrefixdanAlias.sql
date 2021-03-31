623483e53973e7d6c12777972784df504aeb09a8
bani adam afandi <45775790+BaniAdamAfandi@users.noreply.github.com>
2021-03-31 09:29:12 +0700

    Update MenggabungkanPrefixdanAlias.sql

--- MenggabungkanPrefixdanAlias.sql
+++ MenggabungkanPrefixdanAlias.sql
@@ -16,3 +16,15 @@ Tugas:
 Tampilkan kolom harga dari tabel ms_produk dengan nama alias harga_jual lengkap dengan prefix.
 SELECT ms_produk.harga AS harga_jual FROM ms_produk 				
 Jika berjalan dengan lancar, Live Code Editor akan mengeluarkan hasil sebagai berikut.
+
+Kesimpulan
+Aku kembali mengambil catatanku dan menuliskan apa yang telah aku pelajari:
+
+Perintah SELECT dapat ditulis dengan variasi identitas kolom dan tabel berupa prefix dan alias.
+Penulisan lengkap untuk nama kolom adalah prefix berupa nama tabel disertai tanda titik sebelum nama kolom itu sendiri.
+Alias adalah nama lain yang diberikan untuk kolom maupun tabel.
+Alias dapat digunakan dengan keyword AS atau tanpa keyword AS setelah nama kolom dan tabel.
+Prefix nama tabel bisa menggunakan alias.
+Pemahaman mengenai prefix dan alias akan mendorong kemampuan identifikasi tabel maupun kolom yang terlibat untuk perintah SELECT yang lebih kompleks.
+
+ 
