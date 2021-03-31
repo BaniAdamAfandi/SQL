4c222af9666e98dafc4266c979e3d2fa87bcb9f1
bani adam afandi <45775790+BaniAdamAfandi@users.noreply.github.com>
2021-03-31 08:47:24 +0700

    Update RDBMS.sql

--- RDBMS.sql
+++ RDBMS.sql
@@ -33,3 +33,10 @@ Selain itu, juga ada MariaDB yang juga gratis atau open source, IBM DB2, Microso
  
 
 Umumnya RDBMS menggunakan SQL untuk mengakses database dan produk RDBMS tidak hanya satu macam saja tetapi ada berbagai macam produk, maka SQL syntax pun bisa jadi sedikit berbeda untuk setiap produk tersebut. Berikut contoh perbandingan MySQL, Oracle, dan SQLSERVER untuk menampilkan beberapa baris data dari suatu tabel :
+
+Sebagai penyimpan data, sistem database relasional memiliki struktur hirarki objek penyimpanan sebagai berikut:
+
+Database
+Tabel (table)
+Kolom (column) atau Field
+Dari sini aku belajar informasi menarik yaitu setiap database bisa berisi beberapa tabel, dan setiap tabel bisa terdiri dari beberapa kolom. Di setiap database, tabel dan kolom memiliki nama sendiri sebagai identitas mereka. Tabel dan kolom inilah yang akan diisi data yang kemudian membentuk row (baris data). 
