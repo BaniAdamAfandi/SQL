48762f168a9615a60db34460462078790ab38ec1
bani adam afandi <45775790+BaniAdamAfandi@users.noreply.github.com>
2021-03-31 09:21:31 +0700

    Create MenggabungkanPrefixdanAlias.sql

---
+++ MenggabungkanPrefixdanAlias.sql
@@ -0,0 +1,18 @@
+Menggabungkan Prefix dan Alias
+Prefix dan alias juga dapat digunakan secara bersamaan.
+
+
+
+Aku menerapkannya dengan tabel ms_produk, menggunakan prefix nama tabel dan alias untuk merubah nama_produk menjadi nama. 
+
+
+
+Jika berjalan dengan lancar, maka akan muncul hasil berikut.
+
+
+
+ 
+Tugas:
+Tampilkan kolom harga dari tabel ms_produk dengan nama alias harga_jual lengkap dengan prefix.
+SELECT ms_produk.harga AS harga_jual FROM ms_produk 				
+Jika berjalan dengan lancar, Live Code Editor akan mengeluarkan hasil sebagai berikut.
