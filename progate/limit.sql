0ed35ad6517ab99a94e1d34d20140f524f755a7c
bani adam afandi <45775790+BaniAdamAfandi@users.noreply.github.com>
2021-04-24 03:16:06 +0700

    Create limit.sql

---
+++ progate/limit.sql
@@ -0,0 +1,18 @@
+/*
+dibawah "FROM purchases" tambahkan code untuk
+menampilkan maksimum 5 baris hasil
+*/
+
+SELECT *
+FROM purchases
+limit 5;
+
+/*
+dibawah WHERE character_name = "Ninja Ken",
+tambahkan code untuk menampilkan hasil maksimum 10 baris
+*/
+
+SELECT *
+FROM purchases
+WHERE character_name = "Ninja Ken"
+limit 10;
