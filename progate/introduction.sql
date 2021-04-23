2c4b4024e1972a7c1b4b9f5344c6de37e6185111
bani adam afandi <45775790+BaniAdamAfandi@users.noreply.github.com>
2021-04-24 01:39:31 +0700

    Delete introduction.sql

--- progate/introduction.sql
+++
@@ -1,9 +0,0 @@
-Dalam SQL, kita menggunakan klausa SELECT untuk mendapatkan data dari database. SELECT digunakan untuk memilih dan mengakses kolom data yang ingin anda olah.
-
-Biasanya setiap database terdiri dari beberapa tabel. Karena inilah, kita harus menggunakan klausa FROM untuk memilih salah satu tabel tersebut. Dengan menggunakan SELECT & FROM, maka kita dapat secara spesifik memilih kolom tertentu dari tabel tertentu untuk diakses dan diolah.
-
-Untuk "mengakhiri" statement SQL, berikan titik koma di akhir statement tersebut. Anda dapat menulis statement SQL dalam satu baris, tapi agar mudah dibaca, gunakan baris baru untuk setiap pernyataan klausa-nya. SQL tidak case-sensitive, Anda bebas untuk menggunakan huruf besar atau kecil, tetapi kami sarankan agar Anda mengikuti format penulisan seperti gambar dibawah.
-
-contoh :
-select name from data;
-select price from data;
